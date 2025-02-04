f508579c70da5aa633a7e21ede5e583c6c3cf481
DezNyuh <devrh3424@gmail.com>
2025-02-04 16:11:23 -0300

    Update Def_Soma.py

--- Def_Soma.py
+++ Def_Soma.py
@@ -9,7 +9,7 @@ while True:
         numero3 = int(input('Escolha um número: '))
         break 
     except ValueError:
-        print('!! DIGITE UMA ENTRADA VÁLIDA !!')
+        print('!!   DIGITE UMA ENTRADA VÁLIDA   !!')
 
 total = asoma(numero1, numero2, numero3)
 print(total)
