14a13354e88296473970407a03452546aaade089
DezNyuh <devrh3424@gmail.com>
2025-02-05 17:12:27 -0300

    Update Def_saudacao.py

--- Def_saudacao.py
+++ Def_saudacao.py
@@ -1,7 +1,15 @@
-def saudacao(nome, idade):
+def saudacao(idade, nome='Visitante'):
     return f'Olá, {nome}! Você têm {idade} anos!'
 
 seu_nome = input('Qual seu nome? ')
+if not seu_nome:
+    seu_nome = None
+
 sua_idade = int(input('Qual sua idade? '))
-mensagem = saudacao(seu_nome, sua_idade)
+
+if seu_nome is None:
+    mensagem = saudacao(sua_idade)
+else:
+    mensagem = saudacao(sua_idade, seu_nome)
+
 print(mensagem)
