293acd99df449eea4c4eb3bf6e70896cfa722823
DezNyuh <devrh3424@gmail.com>
2025-03-06 17:20:17 -0300

    Mais um teste
    Testando tudo

--- fin/alunos_class.py
+++ fin/alunos_class.py
@@ -1,3 +1,5 @@
+alunos = []
+
 class Aluno:
     def __init__ (self, aluno, nota, status):
         self.aluno = aluno
@@ -5,4 +7,17 @@ class Aluno:
         self.status = status
 
     def cadastro_aluno (self):
-        
+        nome = input('Digite o seu nome: ')
+
+        try:
+            nota = int(input('Digite sua nota: '))
+            if 0 <= nota <= 10:
+                status = 'Aprovado' if nota >= 6 else 'Reprovado'
+                print(f'{nome}, você foi {status}.')
+            else:
+                print('Erro: Nota Inválida, a nota deve estar entre 0 e 10.')
+        except ValueError:
+            print('Erro: Entrada Inválida.')
+
+cadastar_aluno = Aluno('aluno', 'nota', 'status')
+cadastar_aluno.cadastro_aluno()
